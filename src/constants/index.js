cf1a939829774d7de96b157c5b145d63395d8199
heyAbdullahBruh <mr.sayedf@yandex.com>
2026-02-24 17:28:29 +0600

    V-1.001 create index.js, apiResponse.js, error middleware, db.js, slugify.js app.js

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const ROLES = {
+  ADMIN: "ADMIN",
+  MODERATOR: "MODERATOR",
+  USER: "USER",
+};
+
+export const ORDER_STATUS = {
+  PENDING: "Pending",
+  CONFIRMED: "Confirmed",
+  PROCESSING: "Processing",
+  SHIPPED: "Shipped",
+  DELIVERED: "Delivered",
+  CANCELLED: "Cancelled",
+};
+
+export const GENDER_SPECIFIC = {
+  MALE: "Male",
+  FEMALE: "Female",
+  UNISEX: "Unisex",
+};
+
+export const AGE_GROUP = {
+  CHILD: "Child",
+  ADULT: "Adult",
+  OLD: "Old",
+};
