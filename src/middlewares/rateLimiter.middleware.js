c636600c8ce3020f4ddb04148a262032fd708bc2
heyAbdullahBruh <mr.sayedf@yandex.com>
2026-04-02 15:34:45 -0400

    Ratelimit titme change

--- src/middlewares/rateLimiter.middleware.js
+++ src/middlewares/rateLimiter.middleware.js
@@ -34,7 +34,7 @@ const getClientIp = (req) => {
 
 export const apiRateLimiter = rateLimit({
   windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS) || 15 * 60 * 1000,
-  max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 100,
+  max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 600,
   message: {
     success: false,
     message: "Too many requests, please try again later",
